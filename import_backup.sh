f187c0e7fac28a83fe5ff10002aadfef2e6a48cc
Francisco Javier Ferreyra López <pacofigo@outlook.com>
2024-02-03 18:59:41 -0600

    add files
    import_backup

---
+++ import_backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Get system date
+date=$(date +"%d-%b-%Y")
+
+#Remote path
+remote_path="/remote/path"
+
+#Local path
+local_path="/local/path"
+
+#Import remote backup
+sshpass -p "password" scp -P 22 user_name@domain_name:$remote_path/database_nameBackup_$date.tar.gz $local_path/
